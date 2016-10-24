b550fb08011f4e9a4806c1246d78c842577e6f37
Arve Hjønnevåg <arve@android.com>
2016-10-24 15:20:30 +0200

    ANDROID: binder: Clear binder and cookie when setting handle in flat binder struct
    commit 4afb604e2d14d429ac9e1fd84b952602853b2df5 upstream.
    
    Prevents leaking pointers between processes
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    # Conflicts:
    #	drivers/staging/android/binder.c

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2726,7 +2726,9 @@ static void binder_transaction(struct binder_proc *proc,
 				fp->type = BINDER_TYPE_HANDLE;
 			else
 				fp->type = BINDER_TYPE_WEAK_HANDLE;
+			fp->binder = 0;
 			fp->handle = ref->desc;
+			fp->cookie = 0;
 			binder_inc_ref(ref, fp->type == BINDER_TYPE_HANDLE,
 				       &thread->todo);
 
@@ -2772,6 +2774,7 @@ static void binder_transaction(struct binder_proc *proc,
 					return_error = BR_FAILED_REPLY;
 					goto err_binder_get_ref_for_node_failed;
 				}
+<<<<<<< HEAD
 				ref = binder_get_ref_for_node(target_proc, node);
 				if (ref == NULL) {
 #ifdef MTK_BINDER_DEBUG
@@ -2790,6 +2793,14 @@ static void binder_transaction(struct binder_proc *proc,
 				binder_inc_ref(ref, fp->type == BINDER_TYPE_HANDLE, &thread->todo);
 
 				trace_binder_transaction_node_to_ref(t, node, ref);
+=======
+				fp->binder = 0;
+				fp->handle = new_ref->desc;
+				fp->cookie = 0;
+				binder_inc_ref(new_ref, fp->type == BINDER_TYPE_HANDLE, NULL);
+				trace_binder_transaction_ref_to_ref(t, ref,
+								    new_ref);
+>>>>>>> ddfe528... ANDROID: binder: Clear binder and cookie when setting handle in flat binder struct
 				binder_debug(BINDER_DEBUG_TRANSACTION,
 					     "        node %d u%016llx -> ref %d desc %d\n",
 					     node->debug_id, (u64) node->ptr,
@@ -2910,7 +2921,18 @@ static void binder_transaction(struct binder_proc *proc,
 				e->fd = target_fd;
 #endif
 			}
+<<<<<<< HEAD
 			break;
+=======
+			task_fd_install(target_proc, target_fd, file);
+			trace_binder_transaction_fd(t, fp->handle, target_fd);
+			binder_debug(BINDER_DEBUG_TRANSACTION,
+				     "        fd %d -> %d\n", fp->handle, target_fd);
+			/* TODO: fput? */
+			fp->binder = 0;
+			fp->handle = target_fd;
+		} break;
+>>>>>>> ddfe528... ANDROID: binder: Clear binder and cookie when setting handle in flat binder struct
 
 		default:
 			binder_user_error
